1b670531e514f9a93ae8f03d071a49637c3e222d
Taha Hassan <tahafhassan@gmail.com>
2023-03-18 15:16:53 +0000

    Add containers and overlines, make a product component

--- src/components/Overline.tsx
+++ src/components/Overline.tsx
@@ -1,7 +1,9 @@
-const Overline = () => {
-  return (
-    <div className='Overline'>Overline</div>
-  )
+interface Props {
+  heading: string;
 }
+
+const Overline = ({ heading }: Props) => {
+  return <h2 className="Overline">{heading}</h2>;
+};
 
 export default Overline;
--- src/components/ProductQuantity.tsx
+++ src/components/ProductQuantity.tsx
@@ -1,13 +1,18 @@
+import Overline from "./Overline";
+
 interface Props {
   CssClass?: string;
 }
 const ProductQuantity = ({ CssClass }: Props) => {
   const quant = 0;
   return (
-    <div className={"ProductQuantity row " + CssClass}>
-      <button className="btn btn-quantity minus">-</button>
-      {quant}
-      <button className="btn btn-quantity plus">+</button>
+    <div className={CssClass}>
+      <Overline heading="Quantity" />
+      <div className="row space-evenly">
+        <button className="btn btn-quantity minus">-</button>
+        {quant}
+        <button className="btn btn-quantity plus">+</button>
+      </div>
     </div>
   );
 };
