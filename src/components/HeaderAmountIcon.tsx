c4873800ee8c55fc14cc75e17f30eddeaa0c23fd
Taha <98838967+Taha-Hassan-Git@users.noreply.github.com>
2023-04-07 15:46:32 +0100

    header icon updates using reduce method

--- src/components/HeaderAmountIcon.tsx
+++ src/components/HeaderAmountIcon.tsx
@@ -3,11 +3,20 @@ import { IBasketItem } from "../types/types";
 interface Props {
   basket: IBasketItem[];
 }
+function getTotalQuantity(basket: IBasketItem[]): number {
+  return basket.reduce((total, currentProduct) => {
+    return total + currentProduct.quantity;
+  }, 0);
+}
 
 const HeaderAmountIcon = ({ basket }: Props) => {
   return (
-    <div className={`AmountIcon ${0 === 0 && "AmountIcon--inactive"}`}>
-      <p className="Amount">{0}</p>
+    <div
+      className={`AmountIcon ${
+        getTotalQuantity(basket) === 0 && "AmountIcon--inactive"
+      }`}
+    >
+      <p className="Amount">{getTotalQuantity(basket)}</p>
     </div>
   );
 };
