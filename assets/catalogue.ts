f1635ec9bd4a2472cde9885258598bed810c26a1
Taha <98838967+Taha-Hassan-Git@users.noreply.github.com>
2023-04-08 14:51:37 +0100

    Updates

---
+++ assets/catalogue.ts
@@ -0,0 +1,16 @@
+export const catalogue= {
+    WaxedJacket: {
+    id: 1234,
+    name: "Waxed Cotton Hooded Jacket",
+    description: "A beautiful jacket",
+    img: "./public/assets/Jacket.jpg",
+    price: 650,
+  },
+CottonSkirt: {
+    id: 5678,
+    name: "Black Cotton Skirt",
+    description: "A lovely skirt",
+    img: "./public/assets/CottonSkirt.png",
+    price: 325,
+  }
+}
