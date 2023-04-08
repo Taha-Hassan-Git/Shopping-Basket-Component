924ea18a9317ca01306fc90ab90063a83a3b4cbd
Taha <98838967+Taha-Hassan-Git@users.noreply.github.com>
2023-04-08 21:38:26 +0100

    Updates

--- assets/catalogue.ts
+++ assets/catalogue.ts
@@ -3,14 +3,14 @@ export const catalogue= {
     id: 1234,
     name: "Waxed Cotton Hooded Jacket",
     description: "A beautiful jacket",
-    img: "./public/assets/Jacket.jpg",
+    img: "https://cdn-images.farfetch-contents.com/15/48/17/94/15481794_30254686_1000.jpg",
     price: 650,
   },
 CottonSkirt: {
     id: 5678,
     name: "Black Cotton Skirt",
     description: "A lovely skirt",
-    img: "./public/assets/CottonSkirt.png",
+    img: "https://cdn-images.farfetch-contents.com/18/88/17/95/18881795_41509676_1000.jpg",
     price: 325,
   }
 }
